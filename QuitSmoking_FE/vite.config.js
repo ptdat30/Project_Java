a74d7bfc6afcaae2aac77629bf1bbbe3a1a6ccaa
Dat <huynhphongdat2005@gmail.com>
2025-06-14 01:08:17 +0700

    update cố đình port: 4173

--- QuitSmoking_FE/vite.config.js
+++ QuitSmoking_FE/vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite' // <-- Đảm bảo bạn đã cài đặt plugin này nếu sử dụng.
 
 export default defineConfig({
-  plugins: [react(),tailwindcss(),],
+  plugins: [react(), tailwindcss(),],
+  server: {
+    port: 4173,
+  },
 })
