190cbf75d90b1b95d0016c241bb05b9e05a78a8b
Huỳnh Phong Đạt <huynhphongdat2005@gmail.com>
2025-06-14 13:23:10 +0700

    Delete QuitSmoking_BE directory

--- QuitSmoking_BE/quitsmoking/src/main/java/com/quitsmoking/model/AuthProvider.java
+++
@@ -1,6 +0,0 @@
-package com.quitsmoking.model;
-
-public enum AuthProvider {
-    LOCAL, // Đăng nhập bằng tài khoản nội bộ
-    GOOGLE, // Đăng nhập bằng Google
-}
--- QuitSmoking_BE/quitsmoking/src/main/java/com/quitsmoking/model/interfaces/iMemberManageable.java
+++
@@ -1,9 +0,0 @@
-package com.quitsmoking.model.interfaces;
-
-public interface iMemberManageable {
-    void viewMemberProgress(String memberId); // Xem tiến độ của Member
-    void assignCourseToMember(String memberId, String courseId); // Gán khóa học cho Member
-    void sendMessageToMember(String memberId, String message); // Gửi tin nhắn cho Member
-    //...
-    
-}
