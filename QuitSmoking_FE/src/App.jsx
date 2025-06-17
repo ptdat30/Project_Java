40b053ee2e3ebc93bf5c173b720484b3e53f6dcf
LeHoanNguyenVu <vuman6699@gmail.com>
2025-06-17 18:02:45 +0700

    Add new images and ghinhantinhtrang.jsx

--- QuitSmoking_FE/src/App.jsx
+++ QuitSmoking_FE/src/App.jsx
@@ -3,20 +3,25 @@ import { Routes, Route } from "react-router-dom";
 import LoginPage from "./auth/LoginPage";
 import RegisterPage from "./auth/RegisterPage";
 import HomePage from "./components/HomePage";
+import LapKeHoach from './components/lapkehoach';
 
+// const App = () => {
+//   return (
+//     <Routes>
+//       <Route path="/" element={<LoginPage />} />
+//       <Route path="/register" element={<RegisterPage />} />
+//       <Route path="/home" element={<HomePage />} />
+//       {/* Có thể giữ lại route dashboard nếu cần */}
+//       {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+//     </Routes>
+//   );
 const App = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<LoginPage />} />
-      <Route path="/register" element={<RegisterPage />} />
-      <Route path="/home" element={<HomePage />} />
-      {/* Có thể giữ lại route dashboard nếu cần */}
-      {/* <Route path="/dashboard" element={<Dashboard />} /> */}
-    </Routes>
-  );
-};
-
+  return <LapKeHoach />;
+}
 export default App;
+
+
+
 // import React, { useEffect } from "react"; // Import useEffect
 // import { Routes, Route, Navigate } from "react-router-dom"; // Import Navigate
 // import LoginPage from "./auth/LoginPage";
